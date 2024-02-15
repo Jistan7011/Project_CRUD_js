91c73cc21bebbd4358cdd04cb373bb877de0940a
DFItss <101113950+DFItss@users.noreply.github.com>
2024-02-15 15:50:14 +0900

    package.json 수정

--- read_personal_subject.js
+++ read_personal_subject.js
@@ -1,14 +1,13 @@
 let mysql = require('mysql')
-import {connection} from './connection'
 const Input = require('./userInput');
 
-// let connection = mysql.createConnection({
-//   host:process.env.DB_HOST,
-//   port:process.env.DB_PORT,
-//   user:process.env.DB_USER,
-//   password:process.env.DB_PASSWORD,
-//   database:process.env.DB_NAME
-// })
+let connection = mysql.createConnection({
+  host:process.env.DB_HOST,
+  port:process.env.DB_PORT,
+  user:process.env.DB_USER,
+  password:process.env.DB_PASSWORD,
+  database:process.env.DB_NAME
+})
 
 async function read_personal_subject(num){
   console.clear();
@@ -35,12 +34,12 @@ module.exports = {read_personal_subject};
 
 
 // list에 임의의 자료 추가
-// async function list_update(){
+// function list_update(){
 //   for(var i=1; i<6; i++){
 //     for(var j=1; j<5; j++){
 //       for(var k=1; k<4; k++){
 //         let sql = `insert into list values(${i},${(j*100)+k},now())`
-//         // console.log(`${i}/${(j*100)+k}`)
+//         console.log(sql)
 //         connection.query(sql)
 //       }
 //     }
